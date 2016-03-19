d5cf8b0843ef63b37956dd694955590e1a69b1fb
Aaron Chau <aaronchauchau@gmail.com>
2016-03-19 04:42:24 -0700

    Rules page now loads the official HvZ website.

--- app/src/main/java/edu/washington/chau93/hvz_app/models/Game.java
+++ app/src/main/java/edu/washington/chau93/hvz_app/models/Game.java
@@ -10,11 +10,15 @@ import java.util.List;
  * the status of the game, and finally a list of users in the
  * game.
  */
-@JsonIgnoreProperties({"myHumanList", "myZombieList"})
+@JsonIgnoreProperties({"myHumanList", "myZombieList", "myGameUID"})
 public class Game {
+    /** The game unique ID */
     private String myGameUID;
+    /** The game end time in milliseconds. */
     private long myEndTime;
+    /** The game start time in milliseconds. */
     private long myStartTime;
+    /** The game mode/settings. */
     private GameMode myGameGameMode;
     /**
      * GameStatus is a enum with the following:
@@ -24,8 +28,14 @@ public class Game {
      * GameStatus.FINISHED
      */
     private GameStatus gameStatus;
+    /** A list of the user ids. */
     private List<String> myUserIdList;
+    /** A list of the human players. */
     private List<User> myHumanList;
+    /** A list of zombie players. */
     private List<User> myZombieList;
+    /** A flag if the game is private. */
     private boolean myGameIsPrivate;
+    /** The maximun amount of players allowed to play. */
+    private int myMaxPlayers;
 }
