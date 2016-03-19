1255e16a122a925d5349ec77537a3a4de048313c
henrylai <henrylai4@gmail.com>
2016-03-19 12:46:07 -0700

    Added date picker.

--- app/src/main/java/edu/washington/chau93/hvz_app/activities/CreateGameActivity.java
+++ app/src/main/java/edu/washington/chau93/hvz_app/activities/CreateGameActivity.java
@@ -1,12 +1,19 @@
 package edu.washington.chau93.hvz_app.activities;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 import edu.washington.chau93.hvz_app.MapsActivity;
 import edu.washington.chau93.hvz_app.R;
@@ -68,6 +75,31 @@ public class CreateGameActivity extends AppCompatActivity {
             intent.putExtra("GAME_DURATION", Integer.parseInt(myGameDurationField.getText().toString()));
             startActivity(intent);
             finish();
+        }
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
         }
     }
 }
